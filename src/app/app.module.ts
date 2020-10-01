89e1f4f1893b74ad9edb5448317faa9e9a7b225d
martinsmandina <martinsmandina@gmail.com>
2020-10-01 14:07:38 +0300

    create header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GifsComponent } from './gifs/gifs.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GifsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
